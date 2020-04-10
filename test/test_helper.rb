8dcfc0052797a54c8969ffcd6970857bf7ab1fe3
mboya <mboyaberry@gmail.com>
2020-04-10 18:08:57 +0300

    disable http request and activate redis for the gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mpesa_stk"
 require 'webmock/minitest'
-
 require "minitest/autorun"
-Redis.new
 WebMock.disable_net_connect!
+$redis = Redis.new
