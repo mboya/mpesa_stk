7ff803c565223c52c088a893ec509255acbdc71e
mboya <mboyaberry@gmail.com>
2020-05-02 20:44:43 +0300

    pending test for push

--- test/mpesa_stk_test.rb
+++ test/mpesa_stk_test.rb
@@ -27,11 +27,42 @@ class MpesaStkTest < Minitest::Test
     ENV['base_url'] = "https://sandbox"
     ENV['token_generator_url'] = "token/generate"
     stub = stub_request(:get, "#{ENV['base_url']}#{ENV['token_generator_url']}").
-        to_return(status: 200, body: { 'access_token' => 'QWJjcjdhRHNPemFQbGY1Q2Q2RldCSnF4aDZ6UkoyZHk6OVVWek9IaERxNDRua1pXUA==', 'expires_in' => '3599' }.to_json, headers: {})
-    assert_not_requested stub
+        to_return(status: 200, body: {'access_token' => 'QWJjcjdhRHNPemFQbGY1Q2Q2RldCSnF4aDZ6UkoyZHk6OVVWek9IaERxNDRua1pXUA==', 'expires_in' => '3599'}.to_json, headers: {})
+
     token = MpesaStk::AccessToken.call
     assert_requested stub
   end
+
+  # def test_can_push_stk
+  #   $redis.flushall
+  #   ENV['base_url'] = "https://sandbox"
+  #   ENV['token_generator_url'] = "token/generate"
+  #   ENV['process_request_url'] = "/stkpush"
+  #   ENV['business_short_code']=""
+  #   ENV['business_passkey']=""
+  #
+  #   key = "#{ENV['business_short_code']}#{ENV['business_passkey']}#{DateTime.now.strftime("%Y%m%d%H%M%S").to_i}"
+  #   password = Base64.encode64(key).split("\n").join
+  #
+  #   token_stub = stub_request(:get, "#{ENV['base_url']}#{ENV['token_generator_url']}").
+  #       to_return(status: 200, body: {'access_token' => 'QWJjcjdhRHNPemFQbGY1Q2Q2RldCSnF4aDZ6UkoyZHk6OVVWek9IaERxNDRua1pXUA==', 'expires_in' => '3599'}.to_json, headers: {})
+  #   stk_push_stub = stub_request(:post, "#{ENV['base_url']}#{ENV['process_request_url']}").
+  #       with(
+  #           body: "{\"BusinessShortCode\":\"\",\"Password\":\"#{password}=\",\"Timestamp\":\"20200502202935\",\"TransactionType\":\"CustomerPayBillOnline\",\"Amount\":\"10\",\"PartyA\":\"254722111333\",\"PartyB\":\"\",\"PhoneNumber\":\"254722111333\",\"CallBackURL\":\"\",\"AccountReference\":\"zooTu\",\"TransactionDesc\":\"hSMWM\"}",
+  #           headers: {
+  #               'Accept' => '*/*',
+  #               'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+  #               'Authorization' => 'Bearer QWJjcjdhRHNPemFQbGY1Q2Q2RldCSnF4aDZ6UkoyZHk6OVVWek9IaERxNDRua1pXUA==',
+  #               'Content-Type' => 'application/json',
+  #               'User-Agent' => 'Ruby'
+  #           }).
+  #       to_return(status: 200, body: "", headers: {})
+  #
+  #   payment = ::MpesaStk::PushPayment.call('10', '254722111333')
+  #
+  #   assert_requested token_stub
+  #   assert_requested stk_push_stub
+  # end
 
   # def test_can_push_payment
   #   token = "QWJjcjdhRHNPemFQbGY1Q2Q2RldCSnF4aDZ6UkoyZHk6OVVWek9IaERxNDRua1pXUA=="
